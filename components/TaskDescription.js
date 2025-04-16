1f1b2dd773d5aa5e4931f26f078afeaef4d4ab59
arisnoba <arisnoba@gmail.com>
2025-04-16 20:34:53 +0900

    작업 설명 컴포넌트 수정: 설명에 달러 기호가 포함된 경우 텍스트 스타일을 변경하여 시각적 강조를 추가하였습니다. 이를 통해 사용자가 금액 정보를 쉽게 인식할 수 있도록 개선하였습니다.

--- components/TaskDescription.js
+++ components/TaskDescription.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import { memo } from 'react';
 
 const TaskDescription = memo(({ title, description }) => {
+	const hasDollarSign = description?.includes('$');
+
 	return (
 		<div className="w-full h-full flex flex-col">
 			<div className="text-sm text-gray-900 dark:text-gray-100">{title}</div>
-			{description && <div className="mt-1 text-sm text-gray-500 dark:text-gray-400 line-clamp-1 group-hover:text-blue-500 dark:group-hover:text-blue-400">{description}</div>}
+			{description && (
+				<div
+					className={`mt-1 text-sm text-gray-500 dark:text-gray-400 line-clamp-1 ${
+						hasDollarSign ? 'text-yellow-600 dark:text-yellow-500 font-medium' : ''
+					} group-hover:text-blue-500 dark:group-hover:text-blue-400`}>
+					{description}
+				</div>
+			)}
 		</div>
 	);
 });
